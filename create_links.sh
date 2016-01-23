3c53fe2b57a2c9a88627eab844721c09d1e68d95
Andreas Hofmann <mail@andreas-hofmann.org>
2016-01-23 14:07:24 +0100

    create_links.sh: Also handle directories
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- create_links.sh
+++ create_links.sh
@@ -22,11 +22,33 @@ for script in $(find ${SCRIPTPATH} -type f -iname '\.*'); do
 		rm ${old_script}
 	elif [ -f ${old_script} ]; then
 		echo "Moving ${old_script} to ${old_script}.bak"
-		if [ -f ${old_script}.bak ]; then
+		if [ -e ${old_script}.bak ]; then
 			echo "Error: Backup-file already exists!"
 			exit 1
 		fi
 		mv ${old_script} ${old_script}.bak
 	fi
 	ln -s ${SCRIPTPATH}/${script} ${HOME}/
+done
+
+for dir in $(find ${SCRIPTPATH} -type d -iname '\.*'); do
+	dir=$(basename ${dir})
+
+	if [ ${dir} = ".git" ]; then
+		continue
+	fi
+
+	old_dir="${HOME}/${dir}"
+	if [ -h ${old_dir} ]; then
+		echo "Deleting old link to ${dir}"
+		rm ${old_dir}
+	elif [ -d ${old_dir} ]; then
+		echo "Moving ${old_dir} to ${old_dir}.bak"
+		if [ -e ${old_dir}.bak ]; then
+			echo "Error: Backup-dir already exists!"
+			exit 1
+		fi
+		mv ${old_dir} ${old_dir}.bak
+	fi
+	ln -s ${SCRIPTPATH}/${dir} ${HOME}/
 done
