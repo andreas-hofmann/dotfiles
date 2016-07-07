0d9ebb9a8b18ae4eefd17aab55dffa3c88a11bf5
Andreas Hofmann <mail@andreas-hofmann.org>
2016-07-07 20:10:42 +0200

    awesome/rc.lua: Update layouts
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -87,21 +87,37 @@ local layouts = {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
+    awful.layout.suit.max
 }
 -- }}}
 
 -- {{{ Tags
 tags = {
-   names  = { "1:main",   "2:web/mail",    "3:term",   "4:files",  "5:media",  "6:doc",    "7:dev",    "8:vbox", "9:other" },
-   layout = { layouts[2], layouts[2], layouts[6], layouts[4], layouts[1], layouts[2], layouts[2], layouts[10], layouts[1] }
+   names = {
+       "1:main",
+       "2:web/mail",
+       "3:term",
+       "4:files",
+       "5:media",
+       "6:doc",
+       "7:dev",
+       "8:vbox",
+       "9:misc"
+   },
+   layout = {
+       awful.layout.suit.tile,
+       awful.layout.suit.max,
+       awful.layout.suit.fair,
+       awful.layout.suit.tile,
+       awful.layout.suit.floating,
+       awful.layout.suit.tile,
+       awful.layout.suit.tile,
+       awful.layout.suit.max,
+       awful.layout.suit.floating
+   }
 }
+
 for s = 1, screen.count() do
 -- Each screen has its own tag table.
    tags[s] = awful.tag(tags.names, s, tags.layout)
